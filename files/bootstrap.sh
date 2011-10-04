f7ce54a99af0f0bce3a4d8abac9b5abe2a51f2f8
Michael Hale <mike@hales.ws>
2011-10-03 22:50:15 -0400

    use rubygems-1.7.2

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -3,10 +3,11 @@
 if [ ! -f /usr/bin/chef-client ]; then
   apt-get update
   apt-get install -y ruby ruby1.8-dev build-essential wget libruby-extras libruby1.8-extras
+
   cd /tmp
-  wget http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz
-  tar zxf rubygems-1.6.2.tgz
-  cd rubygems-1.6.2
+  wget http://production.cf.rubygems.org/rubygems/rubygems-1.7.2.tgz
+  tar zxf rubygems-1.7.2.tgz
+  cd rubygems-1.7.2
   ruby setup.rb --no-format-executable
 
   gem update --no-rdoc --no-ri
