40a69b95c91ae139b0a95cfeeaa274f824f5ab62
Michael Hale <mike@hales.ws>
2011-10-03 22:49:40 -0400

    enable/disable firewall as network starts/stops

--- files/cookbooks/openswan/recipes/firewall.rb
+++ files/cookbooks/openswan/recipes/firewall.rb
@@ -1,8 +1,19 @@
-execute "apply firewall" do
-  command "iptables-restore < /etc/iptables.rules"
+service "firewall" do
   action :nothing
+  restart_command "iptables-restore < /etc/iptables.rules"
 end
 
 template "/etc/iptables.rules" do
-  notifies :run, "execute[apply firewall]"
+  notifies :restart, "service[firewall]", :immediately
+end
+
+template "/etc/iptables.downrules" do
+end
+
+template "/etc/network/if-pre-up.d/iptablesload" do
+  mode "0755"
+end
+
+template "/etc/network/if-post-down.d/iptablessave" do
+  mode "0755"
 end
